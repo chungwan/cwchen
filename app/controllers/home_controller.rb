545c2198ae839469c72a28be5f73dd40db1eff00
vagrant <vagrant@rails-dev-box>
2014-09-17 00:09:48 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def resume
+  end
+
+  def portfolio
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cwchen::Application.config.secret_token = '1e1e146b8efed284b7662fdbf4afa7928480bb462a2dee9be043071577309b4877c991e81c9975e85671b07cfbfe2d8fee099dfcad2ac83ad0bb1b276c8f97c7'
